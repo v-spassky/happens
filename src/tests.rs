08e89868bbf30b941f8c92a79af41894da8a4daf
Vasiliy Spassky <spassky99@gmail.com>
2023-07-11 15:41:25 +0300

    update table names in tests

--- src/tests.rs
+++ src/tests.rs
@@ -3,21 +3,21 @@ use crate::*;
 #[test]
 fn test_get_substitute_eng_to_ru() {
     let user_input = "qwerty".to_string();
-    let table_name = "eng_to_ru".to_string();
+    let table_name = "eng-to-ru".to_string();
     assert_eq!(get_substitute(user_input, table_name), "йцукен".to_string());
 }
 
 #[test]
 fn test_get_substitute_ru_to_eng() {
     let user_input = "йцукен".to_string();
-    let table_name = "ru_to_eng".to_string();
+    let table_name = "ru-to-eng".to_string();
     assert_eq!(get_substitute(user_input, table_name), "qwerty".to_string());
 }
 
 #[test]
 fn test_get_substitute_mixed() {
     let user_input = "qweЙЦУ".to_string();
-    let table_name = "eng_to_ru".to_string();
+    let table_name = "eng-to-ru".to_string();
     assert_eq!(get_substitute(user_input, table_name), "йцуЙЦУ".to_string());
 }
 
